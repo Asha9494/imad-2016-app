8c934b197629ff591fc887ee0ba65920c9adf524
Asha9494 <ashi09ash@gmail.com>
2016-11-26 06:30:52 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:profile', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
-});
+
 
 app.get('/:about', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'about.html'));
